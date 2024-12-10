e1fe62cb8aa6f3f0f1191d8978345c13a581a50f
rewuugaboo42 <188171707+rewuugaboo42@users.noreply.github.com>
2024-12-10 11:23:46 +0100

    initial commit

--- HPKampen.ino
+++ HPKampen.ino
@@ -3,26 +3,48 @@
 
 MCUFRIEND_kbv tft;
 
+void drawCenteredText(const char* text, int rectX, int rectY, int rectWidth, int rectHeight) {
+  int16_t x1, y1;
+  uint16_t w, h;
+
+  tft.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+
+  int textX = rectX + (rectWidth - w) / 2;
+  int textY = rectY + (rectHeight - h) / 2;
+
+  textY += h / 2;
+
+  tft.setCursor(textX, textY);
+  tft.print(text);
+}
+
 void setup() 
 {
-  // Initialize the TFT
   uint16_t identifier = tft.readID();
   tft.begin(identifier);
 
-  // Set the rotation (0-3 for different orientations)
-  tft.setRotation(1); // Landscape mode
+  tft.setRotation(3);
 
-  // Set the background color
   tft.fillScreen(TFT_BLACK);
 
-  // Draw a rectangle (x, y, width, height, color)
-  tft.drawRect(50, 50, 100, 60, TFT_WHITE);
+  tft.fillRect(90, 10, 300, 140, TFT_WHITE);
 
-  // Fill a rectangle (x, y, width, height, color)
-  tft.fillRect(100, 100, 50, 30, TFT_BLUE);
+  tft.fillRect(15, 170, 220, 70, TFT_WHITE);
+  tft.fillRect(15, 250, 220, 70, TFT_WHITE);
+  tft.fillRect(245, 170, 220, 70, TFT_WHITE);
+  tft.fillRect(245, 250, 220, 70, TFT_WHITE);
+
+  tft.setTextColor(TFT_BLACK);
+  tft.setTextSize(2);
+
+  drawCenteredText("What is 2 + 2?", 90, 10, 300, 140);
+  drawCenteredText("A: 3", 15, 170, 220, 70); 
+  drawCenteredText("B: 4", 15, 250, 220, 70);
+  drawCenteredText("C: 5", 245, 170, 220, 70);
+  drawCenteredText("D: 6", 245, 250, 220, 70);
 }
 
 void loop() 
 {
-  
+
 }
